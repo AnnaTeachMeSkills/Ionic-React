6926194c28b2247edab78ff09abb6aa41e6e1cc7
AnnaTeachMeSkills <khomich10@mail.ru>
2021-07-21 18:32:46 +0300

    added button

--- myApp/src/pages/Home.tsx
+++ myApp/src/pages/Home.tsx
@@ -1,23 +1,19 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import React from 'react';
+
+
+import { IonButton, IonIcon, } from '@ionic/react';
+import { star } from 'ionicons/icons';
+
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+    <IonPage className="App">
+     <div className="ButtonBlock">
+       <IonButton color="secondary" className="mainAPiButton" size="small" >Secondary</IonButton>
+     </div>
     </IonPage>
   );
 };
