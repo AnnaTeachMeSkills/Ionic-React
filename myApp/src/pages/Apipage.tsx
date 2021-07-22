9daec6f1e570f31c45d8be83f0ad8d7f84287c31
AnnaTeachMeSkills <khomich10@mail.ru>
2021-07-22 11:50:39 +0300

    starting to work with API

--- myApp/src/pages/Apipage.tsx
+++ myApp/src/pages/Apipage.tsx
@@ -3,26 +3,30 @@ import { IonList, IonItem, IonLabel, IonInput,IonPage, IonToggle, IonRadio, IonC
 import { useEffect,useState } from 'react';
 import { render } from '@testing-library/react';
 
-const Apipage: React.FC = () => {
-    async function getData  ()  {
-        let response = await fetch('https://anapioficeandfire.com/api/characters/1');
-        console.log(response);
-        let result= await response.json()
-        console.log(result);
-        for(let el in result){
-            console.log(el)
+ const Apipage:  React.FC =  () =>  {
+
+    const [state, setState] = useState({});
+
+     useEffect(  () => {
+        console.log("hello")
+        async function getData() {
+            let response = await fetch('https://anapioficeandfire.com/api/characters/1');
+            let result= await response.json()
+            console.log(result);
+            setState(result);
         }
-        return await result;
-        
-    } 
+        getData()
+    }, [])
 
-    getData()
-
+    console.log(state)
+    let array:string[] = Object.values(state)
+    console.log(array)
+    console.log(array[0])
       return (
         <IonPage>
             <IonList>
                 <IonItem>
-                    <IonLabel>sdf</IonLabel>
+                    <IonLabel>{array[0]}</IonLabel>
                 </IonItem>
                 <IonItem>
                     <IonLabel>Mega Man X</IonLabel>
@@ -44,3 +48,15 @@ const Apipage: React.FC = () => {
 
 
 export default Apipage;
+
+
+
+// array = Object.values(result)
+        // console.log(array)
+        // for(let el in result){
+        // //     console.log(el)
+        // //    console.log(array)
+        // //    array.push(el)
+        // //    console.log(array)
+
+        // }
